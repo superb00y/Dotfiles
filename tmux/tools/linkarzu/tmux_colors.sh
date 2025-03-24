522dbd6e33c801e217cc0de96f946c94e22af298
drider nasreddine <krimodrider@gmail.com>
2025-03-24 20:40:00 +0100

    update tmux config

---
+++ tmux/tools/linkarzu/tmux_colors.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+color0=#18120c
+color1=#f4a034
+color2=#f4a540
+color3=#f5ab4c
+color4=#f6b158
+color5=#f6b664
+color6=#f7bc70
+color7=#d4c4b4
+color8=#504539
+color9=#ffbf76
+color10=#ffc583
+color11=#ffcb90
+color12=#ffd19d
+color13=#ffd7a9
+color14=#ffddb6
+color15=#ede0d4
+
+
