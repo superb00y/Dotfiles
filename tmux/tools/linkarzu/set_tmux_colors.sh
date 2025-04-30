b47c509d50153fb88bebda1e853c928bfa755564
drider nasreddine <krimodrider@gmail.com>
2025-04-30 17:38:18 +0100

    update tmux top bar

--- tmux/tools/linkarzu/set_tmux_colors.sh
+++ tmux/tools/linkarzu/set_tmux_colors.sh
@@ -1,24 +1,73 @@
 #!/usr/bin/env bash
 source "$HOME/.config/tmux/tools/linkarzu/tmux_colors.sh"
 
-tmux set -g @catppuccin_directory_color "$color4"
+# tmux set -g @catppuccin_directory_color "$color4"
 
-tmux set -g @catppuccin_window_current_color "$color2"
-tmux set -g @catppuccin_window_current_background "$color0"
+# tmux set -g @catppuccin_window_current_color "$color2"
+# tmux set -g @catppuccin_window_current_background "$color0"
 
-tmux set -g @catppuccin_window_default_color "$color2"
-tmux set -g @catppuccin_window_default_background "$color0"
+# tmux set -g @catppuccin_window_default_color "$color2"
+# tmux set -g @catppuccin_window_default_background "$color0"
 
-tmux set -g @catppuccin_pane_active_border_style "fg=$color3"
-tmux set -g @catppuccin_pane_border_style "fg=$color0"
+# tmux set -g @catppuccin_pane_active_border_style "fg=$color3"
+# tmux set -g @catppuccin_pane_border_style "fg=$color0"
 
-tmux set -g @catppuccin_status_background "$color0"
+# tmux set -g @catppuccin_status_background "$color0"
 
-tmux set -g @catppuccin_session_color "#{?client_prefix,$color9,$color2}"
+# tmux set -g @catppuccin_session_color "#{?client_prefix,$color9,$color2}"
 
-tmux set -g @catppuccin_window_default_fill "number"
-tmux set -g @catppuccin_window_default_text "#{fg=$color14}#W"
-tmux set -g @catppuccin_window_current_fill "number"
-tmux set -g @catppuccin_window_current_text "#{fg=$color14}#W"
-tmux set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,#[fg=$color4] (    ),}#{?pane_synchronized,#[fg=$color4] SYNCHRONIZED-PANES,}"
+# tmux set -g @catppuccin_window_default_fill "number"
+# tmux set -g @catppuccin_window_default_text "#{fg=$color14}#W"
+# tmux set -g @catppuccin_window_current_fill "number"
+# tmux set -g @catppuccin_window_current_text "#{fg=$color14}#W"
+# tmux set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,#[fg=$color4] (    ),}#{?pane_synchronized,#[fg=$color4] SYNCHRONIZED-PANES,}"
+# 
+# 
 
+HALF_ROUND_OPEN="#(printf '\uE0B6')"
+HALF_ROUND_CLOSE="#(printf '\uE0B4')"
+TRIANGLE_OPEN="#(printf '\uE0B2')"
+TRIANGLE_CLOSE="#(printf '\uE0B0')"
+
+tmux set-option -g status-style "bg=$color0, fg=$color9"
+
+tmux set-option -g status-justify centre
+
+# Style and set contents on the left section
+tmux set-option -g status-left "\
+#[fg=$color8,bg=default]$HALF_ROUND_OPEN\
+#[bg=$color8,fg=$color9]#S \
+#[fg=$color8,bg=default]$TRIANGLE_CLOSE\
+"
+# Style and set contents on the right section
+tmux set-option -g status-right "\
+#[fg=$color8,bg=default]$TRIANGLE_OPEN\
+#[bg=$color8,fg=$color15] #h\
+#[fg=$color8,bg=default]$HALF_ROUND_CLOSE\
+"
+
+# Set max length of left and right section
+tmux set-option -g status-left-length 100
+tmux set-option -g status-right-length 100
+
+# Style and set content for the inactive windows
+tmux set-option -g window-status-format "\
+ \
+#I\
+#[fg=$color12]:\
+#[fg=default]#W\
+ \
+"
+
+# Style and set content for the active windows
+tmux set-option -g window-status-current-format "\
+#[fg=$color8,bg=default]$HALF_ROUND_OPEN\
+#[bg=$color8,fg=default]#I\
+#[fg=$color11]:\
+#[fg=default]#W\
+#[fg=$color8,bg=default]$HALF_ROUND_CLOSE\
+"
+
+# Remove the separator between window list items, as we already have spacing
+# "around" inactive items
+tmux set-option -g window-status-separator ""
--- tmux/tools/linkarzu/tmux_colors.sh
+++ tmux/tools/linkarzu/tmux_colors.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-color0=#18120c
-color1=#f4a034
-color2=#f4a540
-color3=#f5ab4c
-color4=#f6b158
-color5=#f6b664
-color6=#f7bc70
-color7=#d4c4b4
-color8=#504539
-color9=#ffbf76
-color10=#ffc583
-color11=#ffcb90
-color12=#ffd19d
-color13=#ffd7a9
-color14=#ffddb6
-color15=#ede0d4
+color0=#200e0c
+color1=#ff7c6a
+color2=#ff8777
+color3=#ff9284
+color4=#ff9e91
+color5=#ffa99d
+color6=#ffb4aa
+color7=#e9bcb6
+color8=#5f3f3a
+color9=#ffa295
+color10=#ffada2
+color11=#ffb8af
+color12=#ffc4bc
+color13=#ffcfc8
+color14=#ffdad5
+color15=#ffdad5
 
 
